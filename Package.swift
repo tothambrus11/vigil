36f0dcdae8559571ca24de9206db1844c95b410d
Ambrus Toth <ping@ambrus.dev>
2026-05-10 19:59:24 +0300

    experiment: add a Swift macro target
    Adds a trivial #stringify expression macro implemented as a .macro
    target with swift-syntax dependencies, plus a VigilMacroSupport
    library that re-exports it. The vigil executable invokes the macro
    at compile time. If Swift 6.3.1-RELEASE + WindowsExperimental.sdk +
    -static-stdlib breaks .macro target builds on Windows (as suspected
    from hylo-new's failure on ArchivistMacros-tool.exe), this branch's
    CI will fail at the macro plugin link step.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:6.3
 
+import CompilerPluginSupport
 import PackageDescription
 
 let _ = Package(name: "vigil",
@@ -8,15 +9,26 @@ let _ = Package(name: "vigil",
                            branch: "main"),
                   .package(url: "https://github.com/apple/swift-argument-parser",
                            from: "1.6.0"),
+                  .package(url: "https://github.com/apple/swift-syntax",
+                           from: "602.0.0"),
                 ],
                 targets: [
                   .executableTarget(name: "vigil", dependencies: [
                     .product(name: "ArgumentParser", package: "swift-argument-parser"),
                     .product(name: "WindowsCore", package: "swift-platform-core"),
+                    .target(name: "VigilMacroSupport"),
                   ], swiftSettings: [
                     .enableExperimentalFeature("AccessLevelOnImport"),
                   ], plugins: [
                     .plugin(name: "PackageVersion"),
+                  ]),
+                  .target(name: "VigilMacroSupport", dependencies: [
+                    .target(name: "VigilMacros"),
+                  ]),
+                  .macro(name: "VigilMacros", dependencies: [
+                    .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+                    .product(name: "SwiftSyntax", package: "swift-syntax"),
+                    .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                   ]),
                   .plugin(name: "PackageVersion", capability: .buildTool),
                 ])
